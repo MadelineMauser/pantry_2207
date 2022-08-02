d2536d985d44dc3b96f4feddc1bc7e435452b0e8
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 10:15:44 -0600

    Add test "Recipe exists"

---
+++ spec/recipe_spec.rb
@@ -0,0 +1,15 @@
+require './lib/ingredient'
+require './lib/recipe'
+
+RSpec.describe Recipe do
+  before :all do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+  describe 'Recipe instantiation' do
+    it 'exists' do
+      expect(@recipe1).to be_an_instance_of(Recipe)
+    end
+  end
+end
