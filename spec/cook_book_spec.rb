eb5ec378eb74267c2aed68ab0c2d443c28ef013a
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 10:52:07 -0600

    Add test "CookBook exists"

---
+++ spec/cook_book_spec.rb
@@ -0,0 +1,19 @@
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/cook_book'
+
+
+RSpec.describe CookBook do
+  before :all do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @cookbook = CookBook.new
+  end
+  describe 'CookBook instantiation' do
+    it 'exists' do
+      expect(@cookbook).to be_an_instance_of(CookBook)
+    end
+  end
+end
