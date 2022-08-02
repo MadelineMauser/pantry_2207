e5154fba486155d5b6f476af5d1357ddbad1cbc9
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 11:17:41 -0600

    Add test for #total_calories for Recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe Recipe do
   before :all do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
   end
   describe 'Recipe instantiation' do
     it 'exists' do
@@ -34,6 +41,16 @@ RSpec.describe Recipe do
       @recipe1.add_ingredient(@ingredient2, 8)
 
       expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+    end
+  end
+  describe '#total_calories' do
+    it 'can sum the calories of all ingredients' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      expect(@recipe1.total_calories).to eq(440)
+      expect(@recipe2.total_calories).to eq(675)
     end
   end
 end
