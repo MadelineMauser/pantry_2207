4500b39378e8c7a6c741cfbcc48545fe95e8c10f
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 10:40:36 -0600

    Add test for #add_ingredient

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe Recipe do
       expect(@recipe1.ingredients_required).to eq({})
     end
   end
+  describe '#add_ingredient' do
+    it 'can add ingredients to the recipe' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
+    end
+  end
 end
