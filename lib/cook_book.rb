8ab16085b35e032cdcd7f6982ff1f78139057419
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 11:57:04 -0600

    Add #highest_calorie_meal to CookBook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -17,4 +17,13 @@ class CookBook
     end
     ingredients.uniq
   end
+
+  def highest_calorie_meal
+    recipes_calories = Hash.new
+    @recipes.map do |recipe|
+      recipes_calories[recipe] = recipe.total_calories
+    end
+    highest_calorie_meal = recipes_calories.max_by{|key, value| value}[0]
+
+  end
 end
