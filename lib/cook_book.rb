283014ec76cee51412f7f32253bb0b44a2f11058
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-02 11:49:30 -0600

    Add #ingredients to CookBook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,13 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = @recipes.flat_map do |recipe|
+      recipe.ingredients_required.keys.map do |key|
+        key.name
+      end
+    end
+    ingredients.uniq
+  end
 end
